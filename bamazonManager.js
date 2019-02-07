ff8ce6baf60a3b50db7b8da444daa99a3b1f594b
Roman Verner <romverj@gmail.com>
2019-02-07 18:31:37 -0500

    Init manager-view file

---
+++ bamazonManager.js
@@ -0,0 +1,16 @@
+var inquirer = require('inquirer');
+var mysql    = require('mysql');
+
+var connection = mysql.createConnection({
+    host        :   'localhost',
+    port        :   '3306',
+    user        :   'root',
+    password    :   'password',
+    database    :   'bamazon'
+});
+
+connection.connect(err => {
+    if (err) throw err;
+    console.log('Connected as id ' + connection.threadId);
+    connection.end();
+});
