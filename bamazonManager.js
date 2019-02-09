4a9acf506d96cb9c28492061f0767fdb2186b84e
Roman Verner <romverj@gmail.com>
2019-02-09 10:52:19 -0500

    Adding manager view logic

--- bamazonManager.js
+++ bamazonManager.js
@@ -12,5 +12,27 @@ var connection = mysql.createConnection({
 connection.connect(err => {
     if (err) throw err;
     console.log('Connected as id ' + connection.threadId);
+    displayActions();
     connection.end();
 });
+
+var displayActions = function() {
+    inquirer
+    .prompt([
+        {
+            type: 'list',
+            name: 'action',
+            message: 'What would you like to do?',
+            choices: [
+                'View Products for Sale',
+                'View Low Inventory',
+                'Add Inventory',
+                'Add New Product'
+            ]
+        }
+    ])
+    .then(answers => {
+        // Interpret answers as case switches 
+        console.log(answers);
+    });
+};
