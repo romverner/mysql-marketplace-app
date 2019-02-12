c3ae3cbfe98f18d60b045bc5453eca81d1c18e80
Roman Verner <romverj@gmail.com>
2019-02-12 11:36:19 -0500

    Manager view now allows adding new items

--- bamazonManager.js
+++ bamazonManager.js
@@ -42,7 +42,7 @@ var displayActions = function() {
                 addInventory();
                 break;
             case 'Add New Product':
-                // addNew();
+                addNew();
                 break;
         };
     });
@@ -111,4 +111,42 @@ var addInventory = function() {
         }
     );
     connection.end();
+};
+
+var addNew = function() {
+    inquirer
+    .prompt([
+        {
+            name: 'product_name_add',
+            message: 'What is the name of the product?',
+        },
+        {
+            name: 'department_name_add',
+            message: 'What department is the product sold under?'
+        },
+        {
+            name: 'price_name_add',
+            message: 'How much does one quantity of the item cost?'
+        },
+        {
+            name: 'quantity_name_add',
+            message: 'How many of this item are in our stocks?'
+        }
+    ]).then( answers => {
+        var addQuery = 
+            "INSERT INTO products (product_name, department_name, " +
+            "price, stock_quantity) VALUES (?, ?, ?, ?)";
+        var toAdd = [
+            answers.product_name_add,
+            answers.department_name_add,
+            parseFloat(answers.price_name_add),
+            parseInt(answers.quantity_name_add)
+        ];
+        connection.query(addQuery, toAdd, (err, res) => {
+            if (err) throw err;
+            console.log(
+                'Item has been added! Item ID is: ' + res.insertId);
+        });
+        connection.end();
+    });
 };
